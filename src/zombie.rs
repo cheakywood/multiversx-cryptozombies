694c18e6f113b1dbe1271d7c0e6791586421de4c
cheakywood <cheakywood@gmail.com>
2024-11-21 16:49:52 +0100

    Optimize name to reduce cloning overhead

--- src/zombie.rs
+++ src/zombie.rs
@@ -1,8 +1,8 @@
 multiversx_sc::imports!();
 multiversx_sc::derive_imports!();
 
-#[derive(NestedEncode, NestedDecode, TopDecode, TopEncode, TypeAbi)]
+#[derive(NestedEncode, NestedDecode, TopEncode, TopDecode, TypeAbi)]
 pub struct Zombie<M: ManagedTypeApi> {
-    pub dna: u64,
     pub name: ManagedBuffer<M>,
+    pub dna: u64,
 }
--- src/zombie_factory.rs
+++ src/zombie_factory.rs
@@ -1,5 +1,4 @@
 use crate::{ storage, zombie::Zombie };
-
 multiversx_sc::imports!();
 
 #[multiversx_sc::module]
@@ -8,12 +7,18 @@ pub trait ZombieFactory: storage::StorageModule {
     #[endpoint(createZombie)]
     fn create_zombie(&self, owner: ManagedAddress, name: ManagedBuffer, dna: u64) {
         self.zombie_last_index().update(|id: &mut usize| {
-            self.new_zombie_event(*id, name.clone(), dna);
-            let new_zombie: Zombie<<Self as ContractBase>::Api> = Zombie { name, dna };
+            self.new_zombie_event(*id, &name, dna);
+
+            let new_zombie: Zombie<<Self as ContractBase>::Api> = Zombie {
+                name: name.clone(),
+                dna,
+            };
+
             self.owned_zombies(&owner).insert(*id);
             self.zombies(*id).set(new_zombie);
-            *id += 1usize;
-        })
+
+            *id += 1; // Increment ID after storing the zombie
+        });
     }
 
     #[view]
@@ -21,23 +26,25 @@ pub trait ZombieFactory: storage::StorageModule {
         let dna_digits: u8 = self.dna_digits().get();
         let max_dna_value: u64 = (10u64).pow(dna_digits as u32);
 
-        // Generate a random DNA value within the range
-        return RandomnessSource::new().next_u64_in_range(0, max_dna_value);
+        // Generate a random DNA value within the specified range
+        RandomnessSource::new().next_u64_in_range(0, max_dna_value)
     }
 
     #[endpoint(createRandomZombie)]
     fn create_random_zombie(&self, name: ManagedBuffer) {
         let caller: ManagedAddress = self.blockchain().get_caller();
+
         require!(self.owned_zombies(&caller).is_empty(), "You already own a zombie");
-        let dna: u64 = self.generate_random_dna();
-        self.create_zombie(caller, name, dna);
+
+        let random_dna: u64 = self.generate_random_dna();
+        self.create_zombie(caller, name, random_dna);
     }
 
     #[event("newZombieEvent")]
     fn new_zombie_event(
         &self,
         #[indexed] zombie_id: usize,
-        name: ManagedBuffer,
+        name: &ManagedBuffer,
         #[indexed] dna: u64
     );
 }
